bb68e0a86ad4b3db116a7c38943bbd0a8b60ae52
Salies <serezane@protonmail.com>
2023-05-21 21:46:10 -0300

    client

---
+++ client/src/main/java/salies/client/Client.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package salies.client;
+
+/**
+ *
+ * @author Daniel
+ */
+public class Client {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
