7668b6ca0ddc62f61c06a8f245529b4320d5c159
Salies <serezane@protonmail.com>
2023-06-02 00:14:16 -0300

    falta setar as policies

--- runner/src/main/java/salies/runner/ComputeEngine.java
+++ runner/src/main/java/salies/runner/ComputeEngine.java
@@ -1,5 +1,6 @@
 package salies.runner;
 
+import java.rmi.RMISecurityManager;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
@@ -12,14 +13,23 @@ public class ComputeEngine implements Compute {
     public <T> T executeTask(Task<T> t) {
         return t.execute();
     }
-
     public static void main(String[] args) {
         try {
-            String name = "Compute";
+            if (System.getSecurityManager() == null) {
+                System.setSecurityManager(new SecurityManager());
+            }
+
+            System
+
             Compute engine = new ComputeEngine();
+
             Compute stub = (Compute) UnicastRemoteObject.exportObject(engine, 0);
-            Registry registry = LocateRegistry.getRegistry("localhost", Integer.parseInt("1099"));
-            registry.rebind(name, stub);
+
+            // criando o rmiregistry por aqui mesmo
+            Registry registry = LocateRegistry.getRegistry("localhost", 1099);
+
+            registry.rebind("Compute", stub);
+
             System.out.println("ComputeEngine bound");
         } catch (Exception e) {
             System.err.println("ComputeEngine exception:");
--- server/src/main/java/salies/server/Server.java
+++ server/src/main/java/salies/server/Server.java
@@ -1,5 +1,8 @@
 package salies.server;
 
+import salies.server.plotter.Line;
+
+import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -7,6 +10,7 @@ import java.lang.reflect.Array;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.rmi.NotBoundException;
+import java.rmi.RMISecurityManager;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
@@ -17,10 +21,8 @@ public class Server {
     public static void main(String[] args) throws RemoteException, NotBoundException {
         int port = 51666; // Change this to your desired port number
 
-        // Criando ComputeEngine e colocando no rmiregistry
-        String name = "Compute";
-        Registry registry = LocateRegistry.getRegistry("localhost", Integer.parseInt("1099"));
-        comp = (Compute) registry.lookup(name);
+        Registry registry = LocateRegistry.getRegistry("localhost", 1099);
+        comp = (Compute) registry.lookup("Compute");
 
         try {
             ServerSocket serverSocket = new ServerSocket(port);
@@ -61,7 +63,7 @@ public class Server {
             //outputStream.flush();
             // bytes to string
             String input = new String(buffer, 0, bytesRead);
-            parseMessage(input);
+            parseMessage(input, outputStream);
         }
 
         // Cleanup
@@ -72,13 +74,29 @@ public class Server {
     }
 
     // interpreta os comandos recebidos do cliente
-    private static void parseMessage(String message) {
+    private static void parseMessage(String message, OutputStream out) throws IOException {
         String[] args = message.split(" ");
         // Não precisa valida o que vem do cliente. Vamos confiar nele (eu que fiz, pô!)
         int argLen = args.length;
-        switch (args[0]){
+        Task t = null;
+        switch (args[0]) {
             case "plot":
-
+                BufferedImage graph;
+                switch(args[1]) {
+                    case "0":
+                        t = new Line(
+                                Double.parseDouble(args[4]),
+                                Double.parseDouble(args[5]),
+                                Double.parseDouble(args[2]),
+                                Double.parseDouble(args[3]),
+                                1.0f,
+                                "Função afim"
+                        );
+                        break;
+                }
+                graph = (BufferedImage) comp.executeTask(t);
+                String res = "tarefa concluida";
+                out.write(res.getBytes());
                 break;
         }
     }
