b047cd456ae44a93b9f98e0b9d93ae59c024a292
Salies <serezane@protonmail.com>
2023-06-21 21:58:37 -0300

    finalizado

--- client/src/main/java/salies/client/ClientController.java
+++ client/src/main/java/salies/client/ClientController.java
@@ -42,7 +42,7 @@ class ClientController {
         // passo 2: recebe a resposta do servidor - dado o protocolo estabelecido,
         // ela sempre será uma String
         String response = (String) this.inStream.readObject();
-        System.out.println("Recebido o resultado do servidor.");
+        System.out.println("Recebido o resultado do servidor.\n");
 
         // passo 3: mandando a resposta para a MainWindow.
         // Não é responsabilidade do controlador trata-lá, isso é coisa da View.
